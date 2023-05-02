22a70f52639f3c3be78db9a37c74a47cc44dacc9
Estudiante <Estudiante@DESKTOP-F17FT3F>
2023-05-02 12:39:49 +0200

    Esta es la clase principal

---
+++ src/Principal.java
@@ -0,0 +1,10 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Esta es la clase principal de mi proyeco de Industriales
+	}
+
+}
